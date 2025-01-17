7236400d17239d9bec4310cdd9c34007938edd63
Kabe <ivan81747@gmail.com>
2025-01-17 21:00:01 +0300

    завершил курс

--- src/store/functions/updateElement.ts
+++ src/store/functions/updateElement.ts
@@ -1,7 +1,7 @@
 import * as tool from './PresentationType'
 
 function updateElement(slide: tool.SlideType, newElement: tool.TextObjectType): tool.SlideType{
-    console.log('updated fun');
+  console.log('updated fun');  
     return{
         ...slide,
         objects: slide.objects.map(item =>
